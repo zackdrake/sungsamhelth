75346838d2931a09d94641cba50bb227207d6712
Clément <bertonneau@intechinfo.fr>
2020-09-29 11:07:40 +0200

    GravitySensor no bug

--- app/src/main/java/fr/intech/sungsamhelth/MainActivity.java
+++ app/src/main/java/fr/intech/sungsamhelth/MainActivity.java
@@ -2,13 +2,22 @@ package fr.intech.sungsamhelth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
 import android.os.Bundle;
+import android.util.Log;
+
+import fr.intech.sungsamhelth.model.GravitySensor;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Sensor sensor = GravitySensor.getSensor();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("sensor",""+sensor);
+
     }
+
 }
--- app/src/main/java/fr/intech/sungsamhelth/model/GravitySensor.java
+++ app/src/main/java/fr/intech/sungsamhelth/model/GravitySensor.java
@@ -1,15 +1,21 @@
 package fr.intech.sungsamhelth.model;
 
+import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.util.Log;
 
-public class GravitySensor {
+public class GravitySensor extends Activity {
     private SensorManager sensorManager;
-    private Sensor sensor;
+    private static Sensor sensor;
 
     public GravitySensor() {
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+    }
+
+    public static Sensor getSensor() {
+        return sensor;
     }
 }
