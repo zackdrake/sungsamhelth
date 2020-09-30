44ea52a2f57f945428388e132d71369df8af7429
Clément <bertonneau@intechinfo.fr>
2020-09-30 15:26:34 +0200

    step sensor new method

--- app/src/main/java/fr/intech/sungsamhelth/MainActivity.java
+++ app/src/main/java/fr/intech/sungsamhelth/MainActivity.java
@@ -3,21 +3,30 @@ package fr.intech.sungsamhelth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import fr.intech.sungsamhelth.model.GravitySensor;
 
 public class MainActivity extends AppCompatActivity {
-    int NombrePas;
+    private TextView NombrePas;
+    int steps;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        Sensor sensor = GravitySensor.getSensor();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("sensor",""+sensor);
+        NombrePas = findViewById(R.id.NBPas);
+        Sensor sensor = GravitySensor.getSensor();
+        Sensor sensor1 = GravitySensor.getSensor1();
 
+    }
+    public void onSensorChanged(final SensorEvent event) {
+        steps = (int) event.values[0];
+        NombrePas.setText(String.valueOf(steps));
+        Log.i("step",""+steps);
     }
 
 }
--- app/src/main/java/fr/intech/sungsamhelth/model/GravitySensor.java
+++ app/src/main/java/fr/intech/sungsamhelth/model/GravitySensor.java
@@ -3,19 +3,30 @@ package fr.intech.sungsamhelth.model;
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
 import android.util.Log;
 
 public class GravitySensor extends Activity {
     private SensorManager sensorManager;
     private static Sensor sensor;
+    private static Sensor sensor1;
+
 
     public GravitySensor() {
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        sensor1 = sensorManager.getDefaultSensor((Sensor.TYPE_STEP_DETECTOR));
+
     }
 
     public static Sensor getSensor() {
         return sensor;
     }
+
+    public static Sensor getSensor1() {
+        return sensor1;
+    }
+
+
 }
