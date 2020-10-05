ac6cfcc1a7d4d24602955c7ef2d9108fe32c4ea5
Clément <bertonneau@intechinfo.fr>
2020-10-05 19:32:29 +0200

    sleeping finish and small adjustement

--- app/src/main/java/fr/intech/sungsamhelth/MainActivity.java
+++ app/src/main/java/fr/intech/sungsamhelth/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.util.regex.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -24,27 +25,25 @@ import java.util.Date;
 
 import fr.intech.sungsamhelth.model.GravitySensor;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private TextView NombrePas;
     private TextView numberGlass;
     private ImageButton moreGlass;
     private ImageButton lessGlass;
     private ImageButton infoGlass;
-    private EditText tempsSommeil;
-    private TextView sommeil;
+    private ImageButton infoSleep;
+    private ImageButton okSleep;
+    private EditText sleepTime;
+    private TextView sleep;
     int glass = 0;
     int steps;
     String dureeSommeil;
-    CharSequence text = "Il est recommendé de boire 8 verres d'eau par jour.";
-    int duration = Toast.LENGTH_LONG;
     int dayOFTheMonth = Calendar.DAY_OF_MONTH;
     private TextView textViewStepCounter;
     private SensorManager sensorManager;
     private Sensor nStepCounter;
     private boolean isCounterSensorPresent;
     int stepCount = 0;
-
-
 
 
     @Override
@@ -57,12 +56,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!= null)
-        {
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
             nStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
             isCounterSensorPresent = true;
-        }else
-        {
+        } else {
             textViewStepCounter.setText("Step Counter isn't present on your phone");
             isCounterSensorPresent = false;
         }
@@ -71,47 +68,56 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         moreGlass = findViewById(R.id.glassButtonMore);
         lessGlass = findViewById(R.id.glassButtonLess);
         infoGlass = findViewById(R.id.glassButtonInfo);
-        tempsSommeil = findViewById(R.id.sleepingTime);
-        sommeil = findViewById(R.id.sleeping);
+        sleepTime = findViewById(R.id.sleepingTime);
+        sleep = findViewById(R.id.sleepingText);
+        okSleep = findViewById(R.id.sleepingButtonSend);
+        infoSleep = findViewById(R.id.sleepingButtonInfo);
 
         SharedPreferences sharedPref = getSharedPreferences("date", 0);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
         String currentDate = sdf.format(new Date());
-        if (sharedPref.getString("LAST_LAUNCH_DATE","nodate").contains(currentDate)){
-            glass = getPreferences(MODE_PRIVATE).getInt("glassNumber",0);
+        if (sharedPref.getString("LAST_LAUNCH_DATE", "nodate").contains(currentDate)) {
+            glass = getPreferences(MODE_PRIVATE).getInt("glassNumber", 0);
             numberGlass.setText(String.valueOf(glass));
-        }
-        else
-        {
+        } else {
             SharedPreferences.Editor editor = sharedPref.edit();
             editor.putString("LAST_LAUNCH_DATE", currentDate);
             editor.commit();
 
         }
 
-        //dureeSommeil.equals(String.valueOf(tempsSommeil.getText()));
-//         if(dureeSommeil.matches("^([0-9])h([0-5][0-9])$")){
-   //          sommeil.setText(dureeSommeil);
-   //          getPreferences(MODE_PRIVATE).edit().putString("tempsSommeil",dureeSommeil).commit();
-   //      };
+
+        dureeSommeil = sleepTime.getText().toString();
+        okSleep.setOnClickListener(v -> {
+            sleep.setText("aujourdhui vous avez dormi " + dureeSommeil);
+        });
 
         moreGlass.setOnClickListener(v -> {
             glass++;
             numberGlass.setText(String.valueOf(glass));
-            getPreferences(MODE_PRIVATE).edit().putInt("glassNumber",glass).commit();
+            getPreferences(MODE_PRIVATE).edit().putInt("glassNumber", glass).commit();
         });
 
         lessGlass.setOnClickListener(v -> {
             if (glass > 0) {
                 glass--;
                 numberGlass.setText(String.valueOf(glass));
-                getPreferences(MODE_PRIVATE).edit().putInt("glassNumber",glass).commit();
+                getPreferences(MODE_PRIVATE).edit().putInt("glassNumber", glass).commit();
 
             }
         });
 
         infoGlass.setOnClickListener(v -> {
+            CharSequence text = "Il est recommendé de boire 8 verres d'eau par jour.";
+            int duration = Toast.LENGTH_LONG;
             Toast.makeText(this, text, duration).show();
+        });
+
+        infoSleep.setOnClickListener(v -> {
+            CharSequence text = "Il est recommendé de dormir de  7 à 8 heures par jour.";
+            int duration = Toast.LENGTH_LONG;
+            Toast.makeText(this, text, duration).show();
+
         });
         Sensor sensor = GravitySensor.getSensor();
         Sensor sensor1 = GravitySensor.getSensor1();
@@ -119,7 +125,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     public void onSensorChanged(final SensorEvent event) {
-        if(event.sensor == nStepCounter){
+        if (event.sensor == nStepCounter) {
             stepCount = (int) event.values[0];
             textViewStepCounter.setText(String.valueOf(stepCount));
         }
@@ -129,10 +135,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
     @Override
     protected void onResume() {
         super.onResume();
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
             sensorManager.registerListener(this, nStepCounter, SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
@@ -140,7 +147,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onPause() {
         super.onPause();
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
             sensorManager.unregisterListener(this, nStepCounter);
         }
     }
