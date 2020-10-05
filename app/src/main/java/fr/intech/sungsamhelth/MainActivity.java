50de7563ce3e0b6518d8e6701c06b5970139fee5
Clément <bertonneau@intechinfo.fr>
2020-10-05 21:55:48 +0200

    add comm

--- app/src/main/java/fr/intech/sungsamhelth/MainActivity.java
+++ app/src/main/java/fr/intech/sungsamhelth/MainActivity.java
@@ -26,6 +26,7 @@ import java.util.Date;
 import fr.intech.sungsamhelth.model.GravitySensor;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    //Déclaration des variables
     private TextView NombrePas;
     private TextView numberGlass;
     private ImageButton moreGlass;
@@ -49,11 +50,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //lier les variables avec les id dans le activity_main
         setContentView(R.layout.activity_main);
+        textViewStepCounter = findViewById(R.id.textViewStepCounter);
+        numberGlass = findViewById(R.id.NBGlass);
+        moreGlass = findViewById(R.id.glassButtonMore);
+        lessGlass = findViewById(R.id.glassButtonLess);
+        infoGlass = findViewById(R.id.glassButtonInfo);
+        sleepTime = findViewById(R.id.sleepingTime);
+        sleep = findViewById(R.id.sleepingText);
+        okSleep = findViewById(R.id.sleepingButtonSend);
+        infoSleep = findViewById(R.id.sleepingButtonInfo);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        textViewStepCounter = findViewById(R.id.textViewStepCounter);
-
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
@@ -64,15 +73,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             isCounterSensorPresent = false;
         }
 
-        numberGlass = findViewById(R.id.NBGlass);
-        moreGlass = findViewById(R.id.glassButtonMore);
-        lessGlass = findViewById(R.id.glassButtonLess);
-        infoGlass = findViewById(R.id.glassButtonInfo);
-        sleepTime = findViewById(R.id.sleepingTime);
-        sleep = findViewById(R.id.sleepingText);
-        okSleep = findViewById(R.id.sleepingButtonSend);
-        infoSleep = findViewById(R.id.sleepingButtonInfo);
-
+        //garde le nombre de verre et le temps de sommeil sauf si on change de jour
         SharedPreferences sharedPref = getSharedPreferences("date", 0);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
         String currentDate = sdf.format(new Date());
@@ -86,18 +87,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         }
 
-
+        //affichage et changement de la durée du sommeil
         dureeSommeil = sleepTime.getText().toString();
         okSleep.setOnClickListener(v -> {
             sleep.setText("aujourdhui vous avez dormi " + dureeSommeil);
         });
 
+        //affichage et augmentation du nombre de verre
         moreGlass.setOnClickListener(v -> {
             glass++;
             numberGlass.setText(String.valueOf(glass));
             getPreferences(MODE_PRIVATE).edit().putInt("glassNumber", glass).commit();
         });
 
+        //affichage et diminution du nombre de verre
         lessGlass.setOnClickListener(v -> {
             if (glass > 0) {
                 glass--;
@@ -107,20 +110,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         });
 
+        //toast sur le nombre de verres d'eau recommender
         infoGlass.setOnClickListener(v -> {
             CharSequence text = "Il est recommendé de boire 8 verres d'eau par jour.";
             int duration = Toast.LENGTH_LONG;
             Toast.makeText(this, text, duration).show();
         });
 
+        //toast sur le nombre d'heure de sommeil recommender
         infoSleep.setOnClickListener(v -> {
             CharSequence text = "Il est recommendé de dormir de  7 à 8 heures par jour.";
             int duration = Toast.LENGTH_LONG;
             Toast.makeText(this, text, duration).show();
 
         });
-        Sensor sensor = GravitySensor.getSensor();
-        Sensor sensor1 = GravitySensor.getSensor1();
 
     }
 
