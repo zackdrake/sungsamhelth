c0cfd6d92a2d28bd39782ee8ae2174ba2966cac9
Clément <bertonneau@intechinfo.fr>
2020-10-05 12:24:17 +0200

    finish adding cater count

--- app/src/main/java/fr/intech/sungsamhelth/MainActivity.java
+++ app/src/main/java/fr/intech/sungsamhelth/MainActivity.java
@@ -3,6 +3,7 @@ package fr.intech.sungsamhelth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.os.Bundle;
@@ -12,21 +13,25 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
 import fr.intech.sungsamhelth.model.GravitySensor;
 
 public class MainActivity extends AppCompatActivity {
     private TextView NombrePas;
     private TextView numberGlass;
-    private Button moreGlass;
-    private Button lessGlass;
+    private ImageButton moreGlass;
+    private ImageButton lessGlass;
     private ImageButton infoGlass;
     int glass = 0;
     int steps;
-    Context context = getApplicationContext();
-    CharSequence text = "Hello toast!";
-    int duration = Toast.LENGTH_SHORT;
+    CharSequence text = "Il est recommendé de boire 8 verres d'eau par jour.";
+    int duration = Toast.LENGTH_LONG;
 
-    Toast toast = Toast.makeText(context, text, duration);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,27 +43,31 @@ public class MainActivity extends AppCompatActivity {
         lessGlass = findViewById(R.id.glassButtonLess);
         infoGlass = findViewById(R.id.glassButtonInfo);
 
+
         moreGlass.setOnClickListener(v -> {
-            glass ++;
+            glass++;
             numberGlass.setText(String.valueOf(glass));
         });
 
         lessGlass.setOnClickListener(v -> {
-            glass --;
-            numberGlass.setText(String.valueOf(glass));
+            if (glass > 0) {
+                glass--;
+                numberGlass.setText(String.valueOf(glass));
+            }
         });
 
         infoGlass.setOnClickListener(v -> {
-            toast.show();
+            Toast.makeText(this, text, duration).show();
         });
         Sensor sensor = GravitySensor.getSensor();
         Sensor sensor1 = GravitySensor.getSensor1();
 
     }
+
     public void onSensorChanged(final SensorEvent event) {
         steps = (int) event.values[0];
         NombrePas.setText(String.valueOf(steps));
-        Log.i("step",""+steps);
+        Log.i("step", "" + steps);
     }
 
 }
